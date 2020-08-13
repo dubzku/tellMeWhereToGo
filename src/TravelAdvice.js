baa6ab33001a7b06b51bfad144c98cbeb26c45f0
Winnie Ku <winn.ku@gmail.com>
2020-08-12 22:04:27 -0400

    :nail_care: applied styles, re-factored code again, and added API stretch goal

--- src/TravelAdvice.js
+++ src/TravelAdvice.js
@@ -3,20 +3,26 @@ import React from 'react';
 const TravelAdvice = (props) => {
     return (
         <main>
-            <ul className="container">
-                {
-                props.displayedAdvice.map ( (suggestion) => {
-                    return (
-                        <li key={suggestion.key}>
-                            <h2>{suggestion.destination}</h2>
-                            <p>{suggestion.advice}</p>
-                            <img src="" alt=""/>
-                            <button onClick={ () => props.deleteAdvice (suggestion.key) }>Been there!</button>
-                        </li>
-                    )
-                })
-                }
-            </ul>
+            <div className="wrapper">
+                <ul className="container">
+                    {
+                    props.displayedAdvice.map ( (suggestion) => {
+                        return (
+                            <li key={suggestion.key}>
+                                
+                                    <h2>{suggestion.destination}</h2>
+                                    <p>{suggestion.advice}</p>
+                                    <div className="imageContainer">
+                                        <img src={suggestion.photo} alt={suggestion.destination}/>
+                                    </div>
+                                    <button onClick={ () => props.deleteAdvice (suggestion.key) } className="deletePostIt">X</button>
+                                
+                            </li>
+                        )
+                    })
+                    }
+                </ul>
+            </div>
         </main>
     )
 }
